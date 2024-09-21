2857e3fb58fe3ee5e92358dab8de897f1fa3dd2b
Thierry Rotin <thierry.rotin@sociabble.com>
2024-09-21 23:51:32 +0200

    ✨feat: Add new root : Single page vie

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,6 +12,11 @@ const router = createRouter({
       path: '/projects',
       name: 'projects',
       component: () => import('@/views/ProjectsView.vue')
+    },
+    {
+      path: '/projects/:id',
+      name: 'single-project',
+      component: () => import('@/views/SingleProjectView.vue')
     }
   ]
 })
