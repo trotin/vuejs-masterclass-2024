0537137b84d17a52f71d1a79dc29f42c99662ea7
Thierry Rotin <thierry.rotin@sociabble.com>
2024-09-22 01:38:07 +0200

    ✨feat: Remove standard route definition + rename folder views => pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,7 @@
-import { h } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/HomeView.vue')
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('@/views/ProjectsView.vue')
-    },
-    {
-      path: '/projects/:id',
-      name: 'single-project',
-      component: () => import('@/views/SingleProjectView.vue')
-    },
-    {
-      path: '/:catchAll(.*)*',
-      name: 'not-found',
-      component: h('p', { style: 'color: red;' }, '404 Not Found')
-    }
-  ]
+  history: createWebHistory(import.meta.env.BASE_URL)
 })
 
 export default router
