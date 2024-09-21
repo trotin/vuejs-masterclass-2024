7484c669e42f3b796fe3e4ab0b276a12117e7958
Thierry Rotin <thierry.rotin@sociabble.com>
2024-09-21 23:54:20 +0200

    ✨feat: Single page - get dynamic id

--- src/views/SingleProjectView.vue
+++ src/views/SingleProjectView.vue
@@ -1,9 +1,14 @@
 <script setup lang='ts'>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+
 
 </script>
 
 <template>
     <div>
-        <h1>Project Name</h1>
+        <h1>Project {{ route.params?.id }}</h1>
     </div>
 </template>
