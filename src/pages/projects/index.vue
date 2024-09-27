7f130944ef3cba4065da7f22fa0f5292c01bcbc3
Thierry Rotin <thierry.rotin@sociabble.com>
2024-09-27 23:17:17 +0200

    ✨Provide TypeScript Type Definitions for Refs in Vue.js Script Setup

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -2,7 +2,7 @@
 import { supabase } from '@/lib/supabaseClient';
 import { ref } from 'vue';
 
-const projects = ref()
+const projects = ref<any[] | null>(null)
 
 const getProjects = async () => {
     const { data, error } = await supabase.from('projects').select()
@@ -24,7 +24,7 @@ const getProjects = async () => {
         <RouterLink to="/">Back to home</RouterLink>
         <hr />
         <ul>
-            <li v-for="project in projects" :key="project.id">
+            <li v-for="project in projects ?? []" :key="project.id">
                 <RouterLink :to="{ name: '/projects/[id]', params: { id: project.id } }">
                     {{ project.name }}
                 </RouterLink>
