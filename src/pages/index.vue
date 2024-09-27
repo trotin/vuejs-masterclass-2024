812ca08303b0d1932483c3ae0e293509c66b30c0
Thierry Rotin <thierry.rotin@sociabble.com>
2024-09-28 00:48:01 +0200

    ✨ feat: New Tasks page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,5 +6,7 @@
     <div>
         <h1>Hello World</h1>
         <RouterLink to="/projects">Go to projects</RouterLink>
+        <br>
+        <RouterLink to="/tasks">Go to Tasks</RouterLink>
     </div>
 </template>
